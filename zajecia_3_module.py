15dec3c9468b368e8c4317d3af11bebc6552a443
Marcel Tomaszek <marcel.tomaszek@protonmail.com>
2025-10-24 11:44:57 +0200

    next

--- zajecia_3_module.py
+++ zajecia_3_module.py
@@ -1,13 +1,10 @@
 def przywitaj(imie):
-    """Zwraca powitanie dla podanego imienia."""
     return f"Cześć, {imie}!"
 
 
 def podnies_do_kwadratu(liczba):
-    """Podnosi liczbę do kwadratu."""
     return liczba**2
 
 
 def wypisz_liste(elementy):
-    """Łączy elementy listy w napis rozdzielony przecinkami."""
     return ", ".join(str(elem) for elem in elementy)
